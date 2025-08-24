a44d9005f6454db0e7fef1b8941f5ceeabc4469c
recaiguel <133678256+recaiguel@users.noreply.github.com>
2025-08-24 03:18:51 +0200

    brought hash and compare functions to work

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
 
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-  // Store hash in your password DB.
-    console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
 });
 
 //END_ASYNC
